0054cbac5ed5079fb1b431b258c173a082c3e63f
Ousbane Houda <82388160+ousbahou@users.noreply.github.com>
2022-03-14 20:40:03 +0100

    Pages de base
    assets + suppression des pages twenty 1 2 et 3

---
+++ wordpress/wp-content/themes/write_cv/inc/customizer.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * write_cv Theme Customizer
+ *
+ * @package write_cv
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function write_cv_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial(
+			'blogname',
+			array(
+				'selector'        => '.site-title a',
+				'render_callback' => 'write_cv_customize_partial_blogname',
+			)
+		);
+		$wp_customize->selective_refresh->add_partial(
+			'blogdescription',
+			array(
+				'selector'        => '.site-description',
+				'render_callback' => 'write_cv_customize_partial_blogdescription',
+			)
+		);
+	}
+}
+add_action( 'customize_register', 'write_cv_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function write_cv_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function write_cv_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function write_cv_customize_preview_js() {
+	wp_enqueue_script( 'write_cv-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+}
+add_action( 'customize_preview_init', 'write_cv_customize_preview_js' );
