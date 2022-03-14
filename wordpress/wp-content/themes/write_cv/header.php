00b7d011dcd4b0297f6066186b79d44ddbb20879
Luc Boulanger <luc.boulanger98@gmail.com>
2022-03-15 00:13:21 +0100

    debut du header

--- wordpress/wp-content/themes/write_cv/header.php
+++ wordpress/wp-content/themes/write_cv/header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme
  *
@@ -12,8 +13,9 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
@@ -21,39 +23,42 @@
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'write_cv' ); ?></a>
+	<?php wp_body_open(); ?>
+	<div id="page" class="site">
+		<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e('Skip to content', 'write_cv'); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+		<header id="masthead" class="site-header">
+			<div class="site-branding">
 				<?php
-			else :
+				the_custom_logo();
+				if (is_front_page() && is_home()) :
 				?>
-				
+					<h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
 				<?php
-			endif;
-			$write_cv_description = get_bloginfo( 'description', 'display' );
-			if ( $write_cv_description || is_customize_preview() ) :
+				else :
 				?>
-				
-			<?php endif; ?>
-		</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'write_cv' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+				<?php
+				endif;
+				$write_cv_description = get_bloginfo('description', 'display');
+				if ($write_cv_description || is_customize_preview()) :
+				?>
+
+				<?php endif; ?>
+			</div><!-- .site-branding -->
+			<div class="header-nav">
+				<img src="/projCV/wordpress/wp-content/themes/write_cv/assets/img/logo.png" alt="logo" id="logo">
+				<nav id="site-navigation" class="main-navigation">
+					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e('Primary Menu', 'write_cv'); ?></button>
+					<?php
+					wp_nav_menu(
+						array(
+							'theme_location' => 'menu-1',
+							'menu_id'        => 'primary-menu',
+						)
+					);
+					?>
+				</nav>
+			</div>
+			<!-- #site-navigation -->
+		</header><!-- #masthead -->
