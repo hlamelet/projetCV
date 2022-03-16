f87a751861e15d1efb282ab442cbbd671f588a64
Ousbane Houda <82388160+ousbahou@users.noreply.github.com>
2022-03-16 14:09:58 +0100

    Page users
    - interface users

--- wordpress/wp-content/themes/write_cv/assets/js/script.js
+++ wordpress/wp-content/themes/write_cv/assets/js/script.js
@@ -1,1 +1,68 @@
 console.log("bla");
+
+// Rideau d'ouverture partie users
+
+function openNav(){
+    document.getElementById("myNav").style.width = "25%";
+}
+
+function closeNav(){
+    document.getElementById("myNav").style.width= "0%";
+}
+
+// drag & drop
+const base = document.querySelector('.base');
+const box = document.querySelectorAll('.case');
+
+
+
+base.addEventListener('dragstart', dragStart);
+base.addEventListener('dragend', dragEnd);
+
+
+function dragStart() {
+    this.className += ' tenu';
+
+    setTimeout(() => (this.className = 'invisible'), 0);
+}
+
+function dragEnd() {
+    this.className = 'base';
+}
+
+
+for (const vide of box) {
+
+    vide.addEventListener('dragover', dragOver);
+
+    vide.addEventListener('dragenter', dragEnter);
+
+    vide.addEventListener('dragleave', dragLeave);
+
+    vide.addEventListener('drop', dragDrop);
+
+
+}
+
+
+
+function dragOver(e) {
+    e.preventDefault()
+
+
+}
+
+function dragEnter(e) {
+    e.preventDefault();
+    this.className += ' hovered';
+}
+
+function dragLeave() {
+    this.className = 'case';
+}
+
+
+function dragDrop() {
+    this.className = 'case';
+    this.append(base);
+}
--- wordpress/wp-content/themes/write_cv/user_home.php
+++ wordpress/wp-content/themes/write_cv/user_home.php
@@ -5,15 +5,50 @@ Template Name: User Accueil
 */
 
 $db = new PDO('mysql:host=localhost;dbname=cvtheque;charset=utf8', 'root', '');
+wp_head(); ?>
 
-
-get_header(); ?>
 
 <div id="bloc_principal_user">
-    blabla
+
+    <div id="myNav" class="overlay">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+
+
+        <div id="overlay-content1">
+            <a href="#"><img src="<?php echo get_template_directory_uri() ?>/assets/img/logo.png" width="50px" alt=""></a>
+            <a href="#"><i class="fi fi-rr-apps"></i></a>
+            <a href="#"><i class="fi fi-rr-text"></i></a>
+            <a href="#"><i class="fi fi-rr-add"></i></a>
+            <a href=""><i class="fi fi-rr-trash"></i></a>
+        </div>
+
+        <div id="overlay-content2">
+            <div id="barre_recherche">
+                <?php echo do_shortcode('[ivory-search id="18" title="Rechercher des thèmes"]'); ?>
+            </div>
+            <div id="box_templates">
+                <a href="#" class="base" draggable="true"><img src="<?php echo get_template_directory_uri() ?>/assets/img/template1.png" width="auto" alt=""></a>
+                <a href="#"><img src="<?php echo get_template_directory_uri() ?>/assets/img/template2.png" width="auto" alt=""></a>
+                <a href="#"><img src="<?php echo get_template_directory_uri() ?>/assets/img/template1.png" width="auto" alt=""></a>
+                <a href="#"><img src="<?php echo get_template_directory_uri() ?>/assets/img/template1.png" width="auto" alt=""></a>
+            </div>
+        </div>
+        <div id="overlay-content3">
+        </div>
+    </div>
+
+    <span id="bouton_rideau" onclick="openNav()"><i class="fas fa-bars"></i></span>
+    <div id="white"></div>
+    <div id="feuille_cv" class="case">
+    </div>
+    <div id="telechargement">
+
+        <a href="#" id="btn_telecharger">TELECHARGER</a>
+
+    </div>
 </div>
 
 
 
 
-<?php get_footer(); ?>
+<?php wp_footer(); ?>
