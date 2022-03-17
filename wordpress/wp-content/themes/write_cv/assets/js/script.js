0fa320a955a58c659b4ef0826ab372eca73acae9
wtanuki <97055775+wtanuki@users.noreply.github.com>
2022-03-17 21:19:18 +0100

    requete ajax fonctionnelle

--- wordpress/wp-content/themes/write_cv/assets/js/script.js
+++ wordpress/wp-content/themes/write_cv/assets/js/script.js
@@ -1,1 +1,263 @@
-console.log("bla");
+// Rideau d'ouverture partie users
+
+function openNav(){
+    document.getElementById("myNav").style.width = "25%";
+}
+
+function closeNav(){
+    document.getElementById("myNav").style.width= "0%";
+}
+
+// drag & drop
+const base = document.querySelector('.base');
+const box = document.querySelectorAll('.case');
+
+
+
+base.addEventListener('dragstart', dragStart);
+base.addEventListener('dragend', dragEnd);
+
+
+function dragStart() {
+    this.className += ' tenu';
+
+    setTimeout(() => (this.className = 'invisible'), 0);
+}
+
+function dragEnd() {
+    this.className = 'base';
+}
+
+
+for (const vide of box) {
+
+    vide.addEventListener('dragover', dragOver);
+
+    vide.addEventListener('dragenter', dragEnter);
+
+    vide.addEventListener('dragleave', dragLeave);
+
+    vide.addEventListener('drop', dragDrop);
+
+
+}
+
+function dragOver(e) {
+    e.preventDefault()
+
+
+}
+
+function dragEnter(e) {
+    e.preventDefault();
+    this.className += ' hovered';
+}
+
+function dragLeave() {
+    this.className = 'case';
+}
+
+
+function dragDrop() {
+    this.className = 'case';
+    this.append(base);
+}
+
+// THEO
+
+function addNewWEField() {
+    //   console.log("Adding new files");
+  
+    var newNode = document.createElement("textarea");
+    newNode.classList.add("form-control");
+    newNode.classList.add("weField");
+    newNode.classList.add("mt-2");
+    newNode.setAttribute("rows", 3);
+    newNode.setAttribute("placeholder", "Enter here");
+  
+    var weOb = document.getElementById("we");
+    var weAddButtonOb = document.getElementById("weAddButton");
+  
+    weOb.insertBefore(newNode, weAddButtonOb);
+  }
+  
+  function addNewAQField() {
+    var newNode = document.createElement("textarea");
+    newNode.classList.add("form-control");
+    newNode.classList.add("eqField");
+    newNode.classList.add("mt-2");
+    newNode.setAttribute("rows", 3);
+    newNode.setAttribute("placeholder", "Enter here");
+  
+    var aqOb = document.getElementById("aq");
+    var aqAddButtonOb = document.getElementById("aqAddButton");
+  
+    aqOb.insertBefore(newNode, aqAddButtonOb);
+  }
+  
+  //generating cv
+  function generateCV() {
+    // console.log("generating CV");
+  
+    var nameField = document.getElementById("nameField").value;
+  
+    var nameT1 = document.getElementById("nameT1");
+  
+    nameT1.innerHTML = nameField;
+  
+    //direct
+  
+    document.getElementById("nameT2").innerHTML = nameField;
+  
+    //contact
+    document.getElementById("contactT").innerHTML = document.getElementById(
+      "contactField"
+    ).value;
+  
+    //address
+    document.getElementById("addressT").innerHTML = document.getElementById(
+      "addressField"
+    ).value;
+    document.getElementById("fbT").innerHTML = document.getElementById(
+      "fbField"
+    ).value;
+    document.getElementById("instaT").innerHTML = document.getElementById(
+      "instaField"
+    ).value;
+    document.getElementById("linkedT").innerHTML = document.getElementById(
+      "linkedField"
+    ).value;
+  
+    //objective
+  
+    document.getElementById("objectiveT").innerHTML = document.getElementById(
+      "objectiveField"
+    ).value;
+  
+    //we
+  
+    var wes = document.getElementsByClassName("weField");
+  
+    var str = "";
+  
+    for (var e of wes) {
+      str = str + `<li> ${e.value} </li>`;
+    }
+  
+    document.getElementById("weT").innerHTML = str;
+  
+    //aq
+  
+    var aqs = document.getElementsByClassName("eqField");
+  
+    var str1 = "";
+  
+    for (var e of aqs) {
+      str1 += `<li> ${e.value} </li>`;
+    }
+  
+    document.getElementById("aqT").innerHTML = str1;
+  
+    //code for setting image
+  
+    var file = document.getElementById("imgField").files[0];
+  
+    console.log(file);
+  
+    var reader = new FileReader();
+  
+    reader.readAsDataURL(file);
+  
+    console.log(reader.result);
+  
+    //set the image to template
+  
+    reader.onloadend = function () {
+      document.getElementById("imgTemplate").src = reader.result;
+    };
+  
+    document.getElementById("cv-form").style.display = "none";
+    document.getElementById("cv-template").style.display = "block";
+  }
+  
+  //print cv
+  function printCV() {
+    window.print();
+  }
+
+//   TELECHARGEMENT PDF
+
+window.onload = function () {
+    document.getElementById("download")
+        .addEventListener("click", () => {
+            const invoice = this.document.getElementById("invoice");
+            console.log(invoice);
+            console.log(window);
+            var opt = {
+                margin: 1,
+                filename: 'monCv.pdf',
+                image: { type: 'jpeg', quality: 0.98 },
+                html2canvas: { scale: 2 },
+                jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+            };
+            html2pdf().from(invoice).set(opt).save();
+        })
+}
+
+// palette de couleur
+
+var el_up = document.getElementById("GFG_UP");
+var el_down = document.getElementById("GFG_DOWN");
+var str = "Click on button to change the background color";
+
+// el_up.innerHTML = str;
+
+function changeColor(color) {
+    document.getElementById("invoice").style.background = color;
+}
+  
+function gfg_Run() {
+    changeColor('#ffff9a');
+} 
+function blue_Run() {
+    changeColor('#7fb1ff');
+} 
+function pink_Run() {
+    changeColor('rgb(252, 146, 225)');
+} 
+function green_Run() {
+    changeColor('#87ffcc');
+}
+function grey_Run() {
+    changeColor('#8f8f8f');
+} 
+
+$("#submit").click(function() {
+ 
+  var name = $("#nameField").val();
+  var tel = $("#contactField").val();
+
+
+  if(name == '' || tel == '') {
+      alert("Veuillez remplir tous les champs.");
+      return false;
+  }
+
+  $.ajax({
+      type: "POST",
+      url: "/ProjCV/wordpress/wp-content/themes/write_cv/formToBd.php",
+      data: {
+          name: name,
+          tel: tel
+      },
+      cache: false,
+      success: function(data) {
+          alert(data);
+      },
+      error: function(xhr, status, error) {
+          console.error(xhr);
+      }
+  });
+   
+});
+
--- wordpress/wp-content/themes/write_cv/user_home.php
+++ wordpress/wp-content/themes/write_cv/user_home.php
@@ -5,15 +5,163 @@ Template Name: User Accueil
 */
 
 $db = new PDO('mysql:host=localhost;dbname=cvtheque;charset=utf8', 'root', '');
+?>
+
+<div id="bloc_principal_user">
+
+    <div id="myNav" class="overlay">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
 
 
-get_header(); ?>
+        <div id="overlay-content1">
+            <a href="#"><img src="<?php echo get_template_directory_uri() ?>/assets/img/logo.png" width="50px" alt=""></a>
+            <a href="#"><i class="fi fi-rr-apps"></i></a>
+            <a href="#"><i class="fi fi-rr-text"></i></a>
+            <a href="#"><i class="fi fi-rr-add"></i></a>
+            <a href=""><i class="fi fi-rr-trash"></i></a>
+        </div>
+
+        <div id="overlay-content2">
+            <div id="barre_recherche">
+                <?php echo do_shortcode('[ivory-search id="18" title="Rechercher des thèmes"]'); ?>
+            </div>
+            <div id="box_templates">
+                <a href="#" class="base" draggable="true"><img src="<?php echo get_template_directory_uri() ?>/assets/img/template1.png" width="auto" alt=""></a>
+                <!-- couleurs -->
+                <button onclick="gfg_Run()" id="yellow_color"></button>
+                <button onclick="blue_Run()" id="blue_color"></button>
+                <button onclick="green_Run()" id="green_color"></button>
+                <button onclick="grey_Run()" id="grey_color"></button>
+                <button onclick="pink_Run()" id="purple_color"></button>
+
+                <!-- fin couleurs -->
+
+                <a href="#"><img src="<?php echo get_template_directory_uri() ?>/assets/img/template2.png" width="auto" alt=""></a>
+                <a href="#"><img src="<?php echo get_template_directory_uri() ?>/assets/img/template1.png" width="auto" alt=""></a>
+                <a href="#"><img src="<?php echo get_template_directory_uri() ?>/assets/img/template1.png" width="auto" alt=""></a>
+            </div>
+        </div>
+        <div id="overlay-content3">
+        </div>
+    </div>
+
+    <span id="bouton_rideau" onclick="openNav()"><i class="fas fa-bars"></i></span>
+    <div id="white"></div>
+    <div id="feuille_cv">
+
+        <!-- THEO -->
+
+        <div class="card mx-5 mt-3" id="invoice" class="invoice">
+            <div class="card-header">
+
+            </div>
+            <div class="card-body">
+                <div class="container">
+
+                    <div class="row">
+                        <div class="col-md-6">
+                            <!-- premier col -->
+                            <div class="form-group mt-3">
+                                <label for="">Sélectionnez votre photo</label>
+                                <input id="imgField" type="file" class="form-control" />
+                            </div>
+                            <!-- CHANGEMENT -->
+                            <h3 class="text">Renseignements personnels</h3>
+
+                            <div class="form-group">
+                                <label for="nameField"></label>
+                                <input type="text" id="nameField" name="name" placeholder="John Morroy" class="form-control" />
+                            </div>
+
+                            <div class="form-group mt-2">
+                                <label for="contactField"></label>
+                                <input type="text" id="contactField" name="tel" placeholder="0658453623" class="form-control" />
+                            </div>
+
+                            <div class="form-group mt-2">
+                                <label for="addressField"></label>
+                                <textarea id="addressField" placeholder="189 avenue Jean rondeaux 76100 , France" class="form-control" rows="5"></textarea>
+                            </div>
 
 <div id="bloc_principal_user">
     blabla
 </div>
 
+                            <p class="text-secondary my-3">Mes réseaux sociaux</p>
 
+                            <div class="form-group mt-2">
+                                <label for="fbField"></label>
+                                <input type="text" id="fbField" placeholder="Facebook" class="form-control" />
+                            </div>
+                            <div class="form-group mt-2">
+                                <label for="instaField"></label>
+                                <input type="text" id="instaField" placeholder="Instagram" class="form-control" />
+                            </div>
+                            <div class="form-group mt-2">
+                                <label for="linkedField"></label>
+                                <input type="text" id="linkedField" placeholder="LinkedIn" class="form-control" />
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <!-- seconde col -->
+                            <h3>Information professionnelle</h3>
+
+                            <div class="form-group mt-2">
+                                <label for="">OBJECTIFS</label>
+                                <textarea id="objectiveField" rows="5" placeholder="Présentation" class="form-control"></textarea>
+                            </div>
+
+                            <div class="form-group mt-2" id="we">
+                                <label for="">
+                                    EXPERIENCES</label>
+                                <textarea placeholder="Enter here" class="form-control weField" rows="3"></textarea>
+
+                                <!-- textarea -->
+
+                                <div class="container text-center mt-2" id="weAddButton">
+                                    <button onclick="addNewWEField()" class="btn btn-primary btn-sm">
+                                        Ajouter
+                                    </button>
+                                </div>
+                            </div>
+
+                            <div class="form-group mt-2" id="aq">
+                                <label for="">
+                                    FORMATIONS</label>
+                                <textarea placeholder="Enter here" class="form-control eqField" rows="3"></textarea>
+
+                                <div class="container text-center mt-2" id="aqAddButton">
+                                    <button onclick="addNewAQField()" class="btn btn-primary btn-sm" >
+                                        Ajouter
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="container text-center mt-3">
+                        <input value="Créer mon CV" type="button" id="submit" class="btn btn-primary btn-lg" />                      
+                    </div>
+                </div>
+            </div class="card-body">
+
+
+
+            <div class="container mt-3 text-center">
+            </div>
+        </div>
+    </div>
+    <!-- Fin THEO -->
+
+    <div id="telechargement">
+        <button onclick="printCV()" class="btn background">Print CV</button>
+        <button class="btn background" id="download">Télécharger</button>
+    </div>
+
+</div>
+
+
+</body>
 
 
 <?php get_footer(); ?>
