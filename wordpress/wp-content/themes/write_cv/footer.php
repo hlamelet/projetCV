2a8b032bff19fec3707a17426f29b21c11e8be26
Luc Boulanger <luc.boulanger98@gmail.com>
2022-03-18 17:09:56 +0100

    début du footer

--- wordpress/wp-content/themes/write_cv/footer.php
+++ wordpress/wp-content/themes/write_cv/footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer
  *
@@ -11,27 +12,52 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'write_cv' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				?>
+<footer id="colophon" class="site-footer">
+	<div class="footer">
+		<div class="container-footer">
+
+			<div class="Services">
+				<h3>Services</h3>
+				<ul>
+					<li><a href=" #">Web design</a></li>
+					<li><a href="#">Development</a></li>
+					<li><a href="#">Hosting</a></li>
+				</ul>
+			</div>
+			<div class="About">
+				<h3>About</h3>
+				<ul>
+					<li><a href="#">Company</a></li>
+					<li><a href="#">Team</a></li>
+					<li><a href="#">Legacy</a></li>
+				</ul>
+			</div>
+			<div class="Careers">
+				<h3>Careers</h3>
+				<ul>
+					<li><a href="#">Job openings</a></li>
+					<li><a href="#">Employee success</a></li>
+					<li><a href="#">Benefits</a></li>
+				</ul>
+			</div>
+			<div class="Social Media"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-instagram"></i></a>
+				<p class="copyright">Company Name © 2018</p>
 
 
-<!-- FOOTER ICI PROJET  -->
 
-<h5>ICI C'est le footer</h5>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
 
-<?php wp_footer(); ?>
+				<!-- FOOTER ICI PROJET  -->
+
+
+			</div>
+		</div>
+	</div>
+
+</footer><!-- #colophon -->
+
+
+
 
 </body>
+
 </html>
