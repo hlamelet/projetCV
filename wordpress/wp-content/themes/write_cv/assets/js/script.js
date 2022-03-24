eb6e6a3a102eac0ad4524c15c1a3a9c30a5e3b22
wtanuki <97055775+wtanuki@users.noreply.github.com>
2022-03-24 14:50:19 +0100

    remove preview brouillon

--- wordpress/wp-content/themes/write_cv/assets/js/script.js
+++ wordpress/wp-content/themes/write_cv/assets/js/script.js
@@ -107,123 +107,123 @@ const getState = () => {
 	return state;
 };
 
-const buildResume = (state) => {
-	const $ = (value) => {
-		document.write(value);
-	};
+// const buildResume = (state) => {
+// 	const $ = (value) => {
+// 		document.write(value);
+// 	};
 
-	const styleText = `
-@import url('https://fonts.googleapis.com/css?family=Poppins:400,600&display=swap');
+// 	const styleText = `
+// @import url('https://fonts.googleapis.com/css?family=Poppins:400,600&display=swap');
 
-body {
-  font-family: 'Poppins', sans-serif;
-  background: #fafafa;
-  color: rgba(0,0,0,0.75);
-}
+// body {
+//   font-family: 'Poppins', sans-serif;
+//   background: #fafafa;
+//   color: rgba(0,0,0,0.75);
+// }
 
-h1 {
-  color: rgba(0,0,0,0.9);
-}
+// h1 {
+//   color: rgba(0,0,0,0.9);
+// }
 
-h1, p {
-  box-sizing: border-box;
-  margin: 0px;
-  padding: 0px 24px;
-}
+// h1, p {
+//   box-sizing: border-box;
+//   margin: 0px;
+//   padding: 0px 24px;
+// }
 
-.line-break {
-  width: 100%;
-  height: 1px;
-  margin: 16px auto;
-  border-bottom: 1px solid #eee;
-}
+// .line-break {
+//   width: 100%;
+//   height: 1px;
+//   margin: 16px auto;
+//   border-bottom: 1px solid #eee;
+// }
 
-.resume {
-  border-radius: 8px;
-  box-sizing: border-box;
-  display: flex;
-  flex-direction: column;
-  max-width: 800px;
-  margin: 48px auto;
-  padding: 16px 0px;
-  background: white;
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.02), 0 1px 2px rgba(0, 0, 0, 0.14);
-}
+// .resume {
+//   border-radius: 8px;
+//   box-sizing: border-box;
+//   display: flex;
+//   flex-direction: column;
+//   max-width: 800px;
+//   margin: 48px auto;
+//   padding: 16px 0px;
+//   background: white;
+//   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.02), 0 1px 2px rgba(0, 0, 0, 0.14);
+// }
 
-.resume-group {
-  box-sizing: border-box;
-  padding: 8px 0px;
-  width: 100%;
-  display: flex;
-  border-left: 3px solid transparent;
-  transition: 0.2s;
-}
+// .resume-group {
+//   box-sizing: border-box;
+//   padding: 8px 0px;
+//   width: 100%;
+//   display: flex;
+//   border-left: 3px solid transparent;
+//   transition: 0.2s;
+// }
 
-.resume-group:hover {
-  border-left: 3px solid #64FFDA;
-}
+// .resume-group:hover {
+//   border-left: 3px solid #64FFDA;
+// }
 
-.left-col {
-  width: 35%;
-}
+// .left-col {
+//   width: 35%;
+// }
 
-.right-col {
-  width: 65%;
-}
+// .right-col {
+//   width: 65%;
+// }
 
-.instructions {
-  opacity: 0.5;
-  text-align: center;
-  font-size: 0.8rem;
-  margin: 16px auto;
-}
-`;
+// .instructions {
+//   opacity: 0.5;
+//   text-align: center;
+//   font-size: 0.8rem;
+//   margin: 16px auto;
+// }
+// `;
 
-	const createGroup = (left, right) => {
-		$('<div class="resume-group">');
-		$('<div class="left-col">');
-		$("<p>" + left + "</p>");
-		$("</div>");
-		$('<div class="right-col">');
-		$("<p>" + right + "</p>");
-		$("</div>");
-		$("</div>");
-	};
+// 	const createGroup = (left, right) => {
+// 		$('<div class="resume-group">');
+// 		$('<div class="left-col">');
+// 		$("<p>" + left + "</p>");
+// 		$("</div>");
+// 		$('<div class="right-col">');
+// 		$("<p>" + right + "</p>");
+// 		$("</div>");
+// 		$("</div>");
+// 	};
 
-	document.open();
-	$("<html><head>");
-	$("<title>" + state.name + "'s Resume </title>");
-	$("<style>" + styleText + "</style>");
-	$('</head><body><div class="resume">');
-	$("<h1>" + state.name + "</h1>");
-	$("<p>" + state.email + "</p>");
-	$("<p>" + state.phone + "</p>");
-	$("<p>" + state.address + "</p>");
-	$('<div class="line-break"></div>');
-	createGroup("ABOUT ME", state.about);
-	createGroup("CAREER OBJECTIVES", state.career);
-	createGroup("EDUCATION", state.education);
-	createGroup("EMPLOYMENT EXPERIENCE", "");
-	createGroup(
-		state.job1.date.start + " to " + state.job1.date.end,
-		state.job1.details
-	);
-	createGroup(
-		state.job2.date.start + " to " + state.job2.date.end,
-		state.job2.details
-	);
-	createGroup(
-		state.job3.date.start + " to " + state.job3.date.end,
-		state.job3.details
-	);
-	createGroup("REFERENCES", state.references);
-	$("</div>");
-	$(
-		'<div class="instructions">Pour enregistrer votre CV : clic droit > enregistrer sous</div>'
-	);
-	$("</body></html>");
-	document.close();
-};
+// 	document.open();
+// 	$("<html><head>");
+// 	$("<title>" + state.name + "'s Resume </title>");
+// 	$("<style>" + styleText + "</style>");
+// 	$('</head><body><div class="resume">');
+// 	$("<h1>" + state.name + "</h1>");
+// 	$("<p>" + state.email + "</p>");
+// 	$("<p>" + state.phone + "</p>");
+// 	$("<p>" + state.address + "</p>");
+// 	$('<div class="line-break"></div>');
+// 	createGroup("ABOUT ME", state.about);
+// 	createGroup("CAREER OBJECTIVES", state.career);
+// 	createGroup("EDUCATION", state.education);
+// 	createGroup("EMPLOYMENT EXPERIENCE", "");
+// 	createGroup(
+// 		state.job1.date.start + " to " + state.job1.date.end,
+// 		state.job1.details
+// 	);
+// 	createGroup(
+// 		state.job2.date.start + " to " + state.job2.date.end,
+// 		state.job2.details
+// 	);
+// 	createGroup(
+// 		state.job3.date.start + " to " + state.job3.date.end,
+// 		state.job3.details
+// 	);
+// 	createGroup("REFERENCES", state.references);
+// 	$("</div>");
+// 	$(
+// 		'<div class="instructions">Pour enregistrer votre CV : clic droit > enregistrer sous</div>'
+// 	);
+// 	$("</body></html>");
+// 	document.close();
+// };
 
 const checkName = () => {
 	const name = document.getElementById("name");
