09b66cdc7a2b9836d611d01e675a9df00a01d942
hlamelet <hlamelet@gmail.com>
2022-03-25 12:57:47 +0100

    mentions

---
+++ wordpress/wp-content/themes/write_cv/mentionLegal.php
@@ -0,0 +1,84 @@
+<?php
+/*
+Template Name: Mentions Legales
+
+*/
+session_start();
+wp_head();
+get_header()
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        pre {
+            background-color: #21d4fd;
+            background-image: linear-gradient(19deg, #21d4fd 0%, #b721ff 100%);
+            color: white;
+        }
+
+        a {
+            color: white
+        }
+    </style>
+
+</head>
+
+<body>
+    <pre>
+    <h2 style="margin-left: auto;">Mentions légales et politique de confidentialité</h2>
+    <p>La société Bertolucci agency, soucieuse des droits des individus, notamment au regard des
+traitements automatisés et dans une volonté de transparence avec ses clients, a mis en place une politique
+reprenant l’ensemble de ces traitements, des finalités poursuivies par ces derniers ainsi que des moyens d’
+actions à la disposition des individus afin qu’ils puissent au mieux exercer leurs droits.
+Pour toute information complémentaire sur la protection des données personnelles, nous vous invitons à
+consulter le site : <a href="https://www.cnil.fr/" target='_blank'>https://www.cnil.fr/</a></p>
+
+<p>La poursuite de la navigation sur ce site vaut acceptation sans réserve des dispositions et conditions
+d'utilisation qui suivent.
+La version actuellement en ligne de ces conditions d'utilisation est la seule opposable pendant toute la durée
+d'utilisation du site et jusqu'à ce qu'une nouvelle version la remplace.</p>
+
+<h3>Article 1 - Mentions légales</h3>
+<h4>1.1 Site</h4>
+<p>Sophrologie Florence Helloin</p>
+<h4>1.2 Éditeur</h4>
+<p>la société Bertolucci agency
+située : 30 Place Henri Gadeau de Kervill, 76000 Rouen
+n° de téléphone : 0618704998
+adresse mail : agency@bertolucci.com</p>
+<h4>1.3 Hébergeur</h4>
+<p>Sophrologie Florence Helloin est hébergé par Hostinger, dont le siège social est situé 61 Lordou Vironos st.
+6023 Larnaca République de Chypre.
+Article 2 - Accès au site
+L'accès au site et son utilisation sont réservés à un usage strictement personnel. Vous vous engagez à ne pas
+utiliser ce site et les informations ou données qui y figurent à des fins commerciales, politiques, publicitaires
+et pour toute forme de sollicitation commerciale et notamment l'envoi de courriers électroniques non
+sollicités.</p>
+<h3>Article 2 - Accès au site</h3>
+<p>L'accès au site et son utilisation sont réservés à un usage strictement personnel. Vous vous engagez à ne pas
+utiliser ce site et les informations ou données qui y figurent à des fins commerciales, politiques, publicitaires
+et pour toute forme de sollicitation commerciale et notamment l'envoi de courriers électroniques non
+sollicités.</p>
+<h3>Article 3 - Contenu du site</h3>
+<p>Toutes les marques, photographies, textes, commentaires, illustrations, images animées ou non, séquences
+vidéo, sons, ainsi que toutes les applications informatiques qui pourraient être utilisées pour faire fonctionner
+ce site et plus généralement tous les éléments reproduits ou utilisés sur le site sont protégés par les lois en
+vigueur au titre de la propriété intellectuelle.
+Ils sont la propriété pleine et entière de l'éditeur ou de ses partenaires. Toute reproduction, représentation,
+utilisation ou adaptation, sous quelque forme que ce soit, de tout ou partie de ces éléments, y compris les
+applications informatiques, sans l'accord préalable et écrit de l'éditeur, sont strictement interdites. Le fait
+pour l'éditeur de ne pas engager de procédure dès la prise de connaissance de ces utilisations non autorisées
+ne vaut pas acceptation desdites utilisations et renonciation aux poursuites.</p>
+    </pre>
+</body>
+
+
+
+</html>
+<?php get_footer();
